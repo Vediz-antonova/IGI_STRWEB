7c058dffecb5cb6229a8b25c8195f66db27e4e96
Lidia <antonovyss2006@gmail.com>
2025-05-24 00:41:04 +0300

    Add admin in shop

--- IGI/LR5/ZooShopApp/shop/admin.py
+++ IGI/LR5/ZooShopApp/shop/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Supplier)
+class SupplierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'article', 'default_price')
+    filter_horizontal = ('categories', 'suppliers',)
+
+@admin.register(ProductSupply)
+class ProductSupplyAdmin(admin.ModelAdmin):
+    list_display = ('product', 'supplier', 'supply_date', 'quantity', 'unit_price')
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ('product', 'sale_date', 'quantity', 'unit_price', 'total')
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'age', 'email')
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'position', 'age', 'phone')
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date')
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('question', 'created_at')
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'email', 'phone')
+
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('reviewer_name', 'rating', 'created_at')
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'is_active', 'valid_from', 'valid_to')
