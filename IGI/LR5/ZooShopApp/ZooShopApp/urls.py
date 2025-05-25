71274d40d8dac1f34311e132b90766ee7449bce5
Lidia <antonovyss2006@gmail.com>
2025-05-25 22:54:33 +0300

    Fix urls home and about

--- IGI/LR5/ZooShopApp/ZooShopApp/urls.py
+++ IGI/LR5/ZooShopApp/ZooShopApp/urls.py
@@ -20,6 +20,5 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('', include('main.urls')),
 ]
