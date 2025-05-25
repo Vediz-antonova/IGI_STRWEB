7b03aab03c5e450f0579c889e08e4bf624c6bef9
Lidia <antonovyss2006@gmail.com>
2025-05-26 02:39:59 +0300

    Tags for categories

--- IGI/LR5/ZooShopApp/main/views.py
+++ IGI/LR5/ZooShopApp/main/views.py
@@ -1,12 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from goods.models import Category
 
 def index(request):
-    categories = Category.objects.all()
+
     context = {
-        'title': 'Home',
-        'categories' : categories
+        'title': 'Home'
     }
     return render(request, 'main/index.html', context)
 
