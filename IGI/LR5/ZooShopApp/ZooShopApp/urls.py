560568d8873a692a2545685d3f09fcaca2d13128
Lidia <antonovyss2006@gmail.com>
2025-05-25 22:44:20 +0300

    Home and about

--- IGI/LR5/ZooShopApp/ZooShopApp/urls.py
+++ IGI/LR5/ZooShopApp/ZooShopApp/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from goods import urls
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('goods.urls')),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- IGI/LR5/ZooShopApp/main/views.py
+++ IGI/LR5/ZooShopApp/main/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'title': 'Home'
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'main/about.html', context)
