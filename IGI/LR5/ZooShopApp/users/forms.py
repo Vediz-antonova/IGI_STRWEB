4cf90d8ff51a0fd07353659bfd5b3bf5adaed7ed
Lidia <antonovyss2006@gmail.com>
2025-05-26 13:02:28 +0300

    Add login

---
+++ IGI/LR5/ZooShopApp/users/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from users.models import User
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label = 'Name',
+        widget=forms.TextInput(attrs={"autofocus": True,
+                                      'class': 'form-control',
+                                      'placeholder': 'Введите имя пользователя'})
+    )
+    password = forms.CharField(
+        label = 'Password',
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+                                          'class': 'form-control',
+                                          'placeholder': 'Введите пароль'})
+    )
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+# class UserRegistrationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = (
+#             "first_name",
+#             "last_name",
+#             "username",
+#             "email",
+#             "password1",
+#             "password2",
+#         )
+#
+#     first_name = forms.CharField()
+#     last_name = forms.CharField()
+#     username = forms.CharField()
+#     email = forms.CharField()
+#     password1 = forms.CharField()
+#     password2 = forms.CharField()
+#
+# class ProfileForm(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields = (
+#             "image",
+#             "first_name",
+#             "last_name",
+#             "username",
+#             "email",)
+#
+#     image = forms.ImageField(required=False)
+#     first_name = forms.CharField()
+#     last_name = forms.CharField()
+#     username = forms.CharField()
+#     email = forms.CharField()
--- IGI/LR5/ZooShopApp/users/views.py
+++ IGI/LR5/ZooShopApp/users/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.urls import reverse
+from users.forms import UserLoginForm
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))  # Исправлено: HttpResponseRedirect
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'Login',
+        'form': form,
     }
-    return render(request, 'users/login.html')
+    return render(request, 'users/login.html', context)
 
 def registration(request):
     context = {
