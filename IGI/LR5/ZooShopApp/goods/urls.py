38f4a5f132b6522fcf4ce4668d1acb6a2bc1169c
Lidia <antonovyss2006@gmail.com>
2025-05-26 05:42:49 +0300

    Search

--- IGI/LR5/ZooShopApp/goods/urls.py
+++ IGI/LR5/ZooShopApp/goods/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from goods import views
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='catalog'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- IGI/LR5/ZooShopApp/goods/views.py
+++ IGI/LR5/ZooShopApp/goods/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 
 from .models import Product
+from .utils import q_search
 
-def catalog(request, category_slug):
+
+def catalog(request, category_slug=None):
     page=request.GET.get('page', 1)
     order_by = request.GET.get('order_by', None)
+    query=request.GET.get('q', None)
 
     if category_slug == 'all':
         products = Product.objects.all()
+    elif query:
+        products = q_search(query)
     else:
         products = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
