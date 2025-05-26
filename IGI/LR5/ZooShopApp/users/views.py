0efeead342b186c59e213b5ad105efd38eec90aa
Lidia <antonovyss2006@gmail.com>
2025-05-26 06:11:15 +0300

    Add pages for users

--- IGI/LR5/ZooShopApp/users/views.py
+++ IGI/LR5/ZooShopApp/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    context = {
+        'title': 'Login',
+    }
+    return render(request, 'users/login.html')
+
+def registration(request):
+    context = {
+        'title': 'Registration',
+    }
+    return render(request, 'users/registration.html')
+
+def profile(request):
+    context = {
+        'title': 'Profile',
+    }
+    return render(request, 'users/profile.html')
+
+def logout(request):
+    ...
