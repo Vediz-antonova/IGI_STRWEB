f58c4bda915c9d42303b82a97734aeb101d27d9d
Lidia <antonovyss2006@gmail.com>
2025-05-26 03:05:09 +0300

    Urls products

--- IGI/LR5/ZooShopApp/goods/urls.py
+++ IGI/LR5/ZooShopApp/goods/urls.py
@@ -4,4 +4,5 @@ from goods import views
 urlpatterns = [
     path('', views.catalog, name='catalog'),
     path('product/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
