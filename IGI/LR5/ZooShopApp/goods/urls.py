2faa60b644ffc3fd318fa8fcf6e942837e4b0c43
Lidia <antonovyss2006@gmail.com>
2025-05-26 05:11:58 +0300

    Filter

--- IGI/LR5/ZooShopApp/goods/urls.py
+++ IGI/LR5/ZooShopApp/goods/urls.py
@@ -3,6 +3,5 @@ from goods import views
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='catalog'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='catalog'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- IGI/LR5/ZooShopApp/goods/views.py
+++ IGI/LR5/ZooShopApp/goods/views.py
@@ -3,15 +3,20 @@ from django.core.paginator import Paginator
 
 from .models import Product
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page=request.GET.get('page', 1)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         products = Product.objects.all()
     else:
         products = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
+    if order_by and order_by != 'default':
+        products = products.order_by(order_by)
+
     paginator = Paginator(products, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         'title': 'Catalog',
