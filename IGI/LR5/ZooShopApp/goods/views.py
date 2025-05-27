c8cf45059c3440226d372c2aaff445ce5c2aa86c
Lidia <antonovyss2006@gmail.com>
2025-05-27 17:21:10 +0300

    sorted catalog by name

--- IGI/LR5/ZooShopApp/goods/views.py
+++ IGI/LR5/ZooShopApp/goods/views.py
@@ -16,9 +16,14 @@ def catalog(request, category_slug=None):
     else:
         products = Product.objects.filter(category__slug=category_slug)
 
-    allowed_sorting_fields = ["default_price", "-default_price", "name", "-name"]
+    allowed_sorting_fields = {
+        "default_price": ["default_price", "name"],
+        "-default_price": ["-default_price", "name"],
+        "name": ["name"],
+        "-name": ["-name"],
+    }
     if order_by in allowed_sorting_fields:
-        products = products.order_by(order_by)
+        products = products.order_by(*allowed_sorting_fields[order_by])
 
     paginator = Paginator(products, 3)
     current_page = paginator.get_page(page)
@@ -32,9 +37,7 @@ def catalog(request, category_slug=None):
 
 def product(request, product_slug):
     product = Product.objects.get(slug=product_slug)
-    # prodSupply = ProductSupply.objects.filter(product=product) or None
     context = {
         'product' : product,
-        # 'prodSupply' : prodSupply,
     }
     return render(request, 'goods/product.html', context)
