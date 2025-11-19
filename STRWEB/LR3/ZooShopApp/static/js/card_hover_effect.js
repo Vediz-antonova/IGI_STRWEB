9f19dda3c71fcf5c4e9cfdfac5740c455bce3efa
Lidia <antonovyss2006@gmail.com>
2025-11-19 16:14:07 +0300

    card_hover_effect

---
+++ STRWEB/LR3/ZooShopApp/static/js/card_hover_effect.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".product-card");
+
+  cards.forEach(card => {
+    card.addEventListener("mousemove", e => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+      const rotateX = (y - centerY) / 10;
+      const rotateY = (x - centerX) / 10;
+
+      card.style.transform = `rotateX(${ -rotateX }deg) rotateY(${ rotateY }deg) scale(1.03)`;
+      card.style.boxShadow = `0 12px 24px rgba(0, 0, 0, 0.15)`;
+    });
+
+    card.addEventListener("mouseleave", () => {
+      card.style.transform = "none";
+      card.style.boxShadow = "none";
+    });
+  });
+});
